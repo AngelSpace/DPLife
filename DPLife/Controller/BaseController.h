f4b2043e6b237b8a8d3bb27edfc23327f586557f
yangpeng <yangpeng@i360r.com>
2014-11-08 22:45:44 +0800

    add base controller

---
+++ DPLife/Controller/BaseController.h
@@ -0,0 +1,13 @@
+//
+//  BaseController.h
+//  DPLife
+//
+//  Created by Administrator on 11/8/14.
+//  Copyright (c) 2014 yp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseController : UIViewController
+
+@end
