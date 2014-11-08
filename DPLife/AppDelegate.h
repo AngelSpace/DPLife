74602ecf7595af037f081881f45c1f18a571bcbf
yangpeng <yangpeng@i360r.com>
2014-11-08 22:19:25 +0800

    first commit

---
+++ DPLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DPLife
+//
+//  Created by Administrator on 11/8/14.
+//  Copyright (c) 2014 yp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
